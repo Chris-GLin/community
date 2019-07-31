195d3d8086fedcfc5b1537b86be945920b771358
chris-Glin <1475608350@qq.com>
2019-07-31 16:18:00 +0800

    modify

--- src/main/java/com/cgl/community/controller/AuthorizeController.java
+++ src/main/java/com/cgl/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.cgl.community.dto.AccessTokenDTO;
 import com.cgl.community.dto.GithubUser;
 import com.cgl.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("2699c8efad2313caba8e");
-        accessTokenDTO.setClient_secret("2b9c33e348ab99a755c3a156a9613a0ef48f5f15");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);;
         GithubUser user = githubProvider.getUser(accessToken);
